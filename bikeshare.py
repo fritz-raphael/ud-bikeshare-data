d8215fb89f5fb8e0c2d684f4d7cceb72eb2c6803
Raphael Fritz <raphael.fritz@posteo.de>
2021-10-19 08:25:32 +0200

    Refactor: Select columns for raw data from indices
    The columns for five rows of raw data will now be selected
    from their indices (as opposed to their names).

--- bikeshare.py
+++ bikeshare.py
@@ -22,7 +22,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    print("Pandas-version:", pd.__version__)
+    print("Pandas-version: {}".format(pd.__version__))
     # get user input for city (chicago, new york city, washington).
     # HINT: Use a while loop to handle invalid inputs
     while True:
@@ -133,13 +133,9 @@ def show_raw_data(df):
     while raw_data == 'yes':
         for five_rows in range(0, df.shape[0], 5):
             if 'Gender' in df.columns:
-                print(df[['Start Time', 'End Time', 'Start Station',
-                          'End Station', 'User Type', 'Gender', 'Birth Year']]\
-                          .iloc[five_rows:(five_rows+5)])
+                print(df.iloc[:, 1:8].iloc[five_rows:(five_rows+5)])
             else:
-                print(df[['Start Time', 'End Time', 'Start Station',
-                          'End Station', 'User Type']]\
-                          .iloc[five_rows:(five_rows+5)])
+                print(df.iloc[:, 1:6].iloc[five_rows:(five_rows+5)])
             try:
                 raw_data = input("\nDo you want to see five more rows?\n")
                 if raw_data != 'yes':
